0e3742ff7628b3859b6469588c2d967d483f9267
djm10 <djm9826@hotmail.com>
2023-07-06 11:22:15 -0500

    add CORS to React JS

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.db.database import engine
 from src.db import models
@@ -16,3 +17,15 @@ def index():
 models.Base.metadata.create_all(engine)
 
 app.mount('/images', StaticFiles(directory=config.folder_static_images), name='images')
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)

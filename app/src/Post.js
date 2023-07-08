0c009b457255c558ae724d860f6d8dfae3a9fc23
djm10 <djm9826@hotmail.com>
2023-07-08 11:25:54 -0500

    add API call to delete post

--- app/src/Post.js
+++ app/src/Post.js
@@ -10,6 +10,25 @@ function Post({post}){
         setImageUrl(BASE_URL + post.IMAGE_URL);
     }, [])
 
+    const handleDelete = (event) => {
+        event?.preventDefault()
+        
+        const requestOptions = {
+            method: 'DELETE'
+        }
+
+        fetch(BASE_URL + "post/delete/" + post.ID, requestOptions)
+        .then(response => {
+            if (response.ok) {
+                window.location.reload()
+            }
+            throw response
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="post">
             <img className="post_image" src={imageUrl}/>
@@ -17,7 +36,7 @@ function Post({post}){
                 <div className="post_title">{post.TITLE}</div>
                 <div className="post_creator">By: {post.CREATOR}</div>
                 <div className="post_content">{post.CONTENT}</div>
-                <div className="post_delete"><button onClick={null}>Delete post</button></div>
+                <div className="post_delete"><button onClick={handleDelete}>Delete post</button></div>
             </div>
         </div>
 

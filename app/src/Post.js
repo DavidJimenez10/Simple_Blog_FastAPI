aeed74c56e1217106cb9d99c314f841364f44096
djm10 <djm9826@hotmail.com>
2023-07-07 21:16:09 -0500

    add blog content

--- app/src/Post.js
+++ app/src/Post.js
@@ -13,7 +13,14 @@ function Post({post}){
     return (
         <div className="post">
             <img className="post_image" src={imageUrl}/>
+            <div className="post_content">
+                <div className="post_title">{post.TITLE}</div>
+                <div className="post_creator">By: {post.CREATOR}</div>
+                <div className="post_content">{post.CONTENT}</div>
+                <div className="post_delete"><button onClick={null}>Delete post</button></div>
+            </div>
         </div>
+
     )
 }
 
